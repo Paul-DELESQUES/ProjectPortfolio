520df3dc69fc9094035c79709fc0497c555c5121
Paul-DELESQUES <pldelesques+github@gmail.com>
2024-04-18 11:04:33 +0200

    update prjects

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,28 +5,30 @@ import close from "./close.svg";
 import css from "./css.png";
 import expressjs from "./expressjs.png";
 import figma from "./figma.png";
-import geodrinkers from "./projects/geodrinkers.png";
 import git from "./git.png";
 import github from "./github.png";
 import html from "./html.png";
 import javascript from "./javascript.png";
-import loading from "./projects/loading.png";
-import logo from "./logo.svg";
 import logoLight from "./logo-light.svg";
+import logo from "./logo.svg";
 import menu from "./menu.svg";
 import moon from "./moon.jpg";
 import mysql from "./mysql.png";
 import nodejs from "./nodejs.png";
+import events from "./projects/eventsPlanner.png";
+import geodrinkers from "./projects/geodrinkers.png";
+import loading from "./projects/loading.png";
 import portfolio from "./projects/portfolio.png";
 import reactjs from "./reactjs.png";
 import threejs from "./threejs.svg";
 
 export {
   avatar,
-  blender,
   bg,
+  blender,
   close,
   css,
+  events,
   expressjs,
   figma,
   geodrinkers,
